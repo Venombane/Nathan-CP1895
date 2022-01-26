146d341334e2040e31bc846a910699ae4094b31d
Nathan Atkins <starfighter_ndga@hotmail.com>
2022-01-26 15:00:46 -0330

    added some error checking to the methods

--- Sample Python Code/class_practice.py
+++ Sample Python Code/class_practice.py
@@ -1,6 +1,5 @@
 class Lightbulb:
-    def __init__(self, wattage=0, is_led=False, brand_name="", is_on=False, brightness=0):
-        print("init method is invoked")
+    def __init__(self, wattage: int, is_led: bool, brand_name: str, is_on: bool = False, brightness: int = 0):
         self.wattage = wattage
         self.is_led = is_led
         self.brand_name = brand_name
@@ -8,23 +7,37 @@ class Lightbulb:
         self.brightness = brightness
 
     def turn_on(self):
-        self.is_on = True
-        print("lightbulb is now on")
+        if self.is_on == True:
+            print("Lightbulb is already on")
+            print(self.is_on)
+        else:
+            self.is_on = True
+            print("lightbulb is now on")
+            print(self.is_on)
 
     def turn_off(self):
-        self.is_on = False
-        print("lightbulb is now off")
+        if self.is_on == False:
+            print("Lightbulb is already off")
+            print(self.is_on)
+        else:
+            self.is_on = False
+            print("lightbulb is now off")
+            print(self.is_on)
 
     def to_string(self):
-        print(self.brand_name)
+        print(self.brand_name, self.is_led)
 
-    def set_brightness(self, brightness):
-        self.brightness = brightness
-        print("brightness has been set to ", brightness)
+    def set_brightness(self, brightness_level):
+        assert 0 <= brightness_level <= 10, 'Level outside bounds'
+        self.brightness = brightness_level
+        print("brightness has been set to ", brightness_level)
 
 
 # creating a new object and passing values for the attributes
-a_object = Lightbulb(10, True, "A34 Lightbulb", True, 26)
+a_object = Lightbulb(10, True, "A34 Lightbulb")
 
 # calling the afoo() method from a_obj
 a_object.turn_on()
+a_object.turn_off()
+a_object.to_string()
+a_object.set_brightness(8)
